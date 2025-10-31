a203b9a27a96c9702f226492ba4bea79bd639f55
MN-Hoai <hoaimn.cntt@gmail.com>
2025-10-31 08:50:47 +0700

    ok

---
+++ EmployeeMangement/EmployeeMangement/Controllers/Account/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using CRUD_Product;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Threading.Tasks;
+
+namespace CRUD_Product.Controllers
+{
+    public class AccountController : Controller
+    {
+        //private readonly AccountCommand _accountCommand;
+
+        //public AccountController(AccountCommand accountCommand)
+        //{
+        //    _accountCommand = accountCommand;
+        //}
+
+        // GET: /Account/SignIn
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        //// POST: /Account/SignIn
+        //[HttpPost]
+        //public async Task<IActionResult> SignIn(AccountModel.SignInRequest request)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        ViewBag.Error = "Vui lòng nhập đầy đủ thông tin đăng nhập.";
+        //        return View(request);
+        //    }
+
+        //    var result = await _accountCommand.CheckAccount(request);
+
+        //    if (!result)
+        //    {
+        //        ViewBag.Error = "Tên đăng nhập hoặc mật khẩu không đúng.";
+        //        return View(request);
+        //    }
+
+        //    return RedirectToAction("List", "Product");
+        //}
+
+        //// GET: /Account/Logout
+        //[HttpGet]
+        //public async Task<IActionResult> Logout()
+        //{
+        //    await _accountCommand.Logout();
+        //    return RedirectToAction("SignIn", "Account");
+        //}
+    }
+}
---
+++ EmployeeMangement/EmployeeMangement/Controllers/Employee/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMangement.Controllers
+{
+   
+    public class EmployeeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Header()
+        {
+            return PartialView();
+        }
+        // GET: api/employees
+        [HttpGet("api/employees")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(new { message = "Get all employees" });
+        }
+
+        // GET: api/employees/5
+        [HttpGet("api/employee/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(new { message = $"Get employee {id}" });
+        }
+
+        // POST: api/employees
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] object employee)
+        {
+            return Created("", new { message = "Employee created" });
+        }
+
+        // PUT: api/employees/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] object employee)
+        {
+            return Ok(new { message = $"Employee {id} updated" });
+        }
+
+        // DELETE: api/employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
